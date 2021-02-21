c0a24349ac3643f623ccb6f2173f5808687d001f
koynovbro <konstantin.koynov@gmail.com>
2021-02-21 17:51:57 +0700

    Fix ru lang

--- main.c
+++ main.c
@@ -5,18 +5,18 @@ char figures[] = {'r', 'k', 'b', 'K', 'Q', 'b', 'k', 'r'};
 
 void fillChessboard(char chessboard[8][8])
 {
-    // Заполняем пустотой поле
+    // Zapolnenie pustogo polya
     for (int n = 0; n < 8; n++) {
         for (int m = 0; m < 8; m++) {
             chessboard[n][m] = ' ';
         }
     }
-    // Заполняем пешки
+    // Zapolnenie peshok
     for (int i = 0; i < 8; i++) {
         chessboard[1][i] = 'p';
         chessboard[6][i] = 'p';
     }
-    // Заполняем фигуры
+    // Zapolnenie figur
     for (int i = 0; i < 8; i++) {
         chessboard[0][i] = figures[i];
         chessboard[7][i] = figures[i];
